5df5a4a89e565738357ae34baba0d88b2e605a9a
wilkques <wilkques@gmail.com>
2023-08-02 17:50:52 +0800

    FIX - tests

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -28,34 +28,6 @@ trait CreatesApplication
             $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
         );
 
-        /*
-        |--------------------------------------------------------------------------
-        | Bind Important Interfaces
-        |--------------------------------------------------------------------------
-        |
-        | Next, we need to bind some important interfaces into the container so
-        | we will be able to resolve them when needed. The kernels serve the
-        | incoming requests to this application from both the web and CLI.
-        |
-        */
-
-        $app->singleton(
-            \Illuminate\Contracts\Http\Kernel::class,
-            \App\Http\Kernel::class
-        );
-
-        $app->singleton(
-            \Illuminate\Contracts\Console\Kernel::class,
-            \App\Console\Kernel::class
-        );
-
-        $app->singleton(
-            \Illuminate\Contracts\Debug\ExceptionHandler::class,
-            \App\Exceptions\Handler::class
-        );
-
-        $app->make(Kernel::class)->bootstrap();
-
         return $app;
     }
 }
