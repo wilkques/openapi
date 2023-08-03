9f4163ba261fb18e334f24d39bc1cff2756964c5
wilkques <wilkques@gmail.com>
2023-08-03 17:29:58 +0800

    FIX - tests

--- tests/GeneratorTest.php
+++ tests/GeneratorTest.php
@@ -246,6 +246,7 @@ class GeneratorTest extends TestCase
      */
     public function testHasPaths($docs)
     {
+        dd($this->endpoints, array_keys($docs['paths']));
         $this->assertEquals($this->endpoints, array_keys($docs['paths']));
 
         return $docs['paths'];
